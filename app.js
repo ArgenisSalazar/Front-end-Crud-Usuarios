b8c202badc7cc3e5b6f6c1321ba26859bf8e503c
Argenis Salazar <argenis.salazar.sihuay@gmail.com>
2026-03-29 21:47:24 -0500

    fifth commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const API_URL = 'https://users-crud-api.onrender.com/api';
+const API_URL = 'https://users-crud-api-u3q1.onrender.com';
 
 /* ── Estado de la aplicación ────────────────────────────── */
 const state = {
